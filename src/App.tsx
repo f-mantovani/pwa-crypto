9cbe1b52ab5194b108e21e43b532d5a1d0e30184
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-02-16 02:22:12 +0100

    sort is working now in strict mode too

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { SearchResults } from './components/SearchResults'
 import binnanceConnect from './utils/binanceConnect'
 import { Trades, DayInfo, Sorter } from './utils/types'
 
+const sorter: Sorter = {
+	order: 'desc',
+	lastPick: 'time',
+}
+
 function App() {
 	const [trades, setTrades] = useState<Trades[] | null>(null)
 	const [dayInfo, setDayInfo] = useState<DayInfo | null>(null)
-	const [sorter, setSorter] = useState<Sorter>({
-		order: 'desc',
-		lastPick: 'time',
-	})
 
 	const getPairInfo = async (pair: string) => {
 		const tradesData = await binnanceConnect.getPairTrade(pair)
@@ -24,58 +25,49 @@ function App() {
 	}
 
 	const sortData = (type: Sorter['lastPick']) => {
-		setTrades(prevTrades => {
-			const copy = [...(prevTrades || [])]
-
-			if (type === sorter.lastPick) {
-				setSorter(prevSorter => {
-					const newSorter = { ...prevSorter }
-					if (newSorter.order === 'asc') {
-						newSorter.order = 'desc'
-					} else {
-            newSorter.order = 'asc'
-          }
-
-          return newSorter
-				})
+		const copy = [...(trades || [])]
+		if (type === sorter.lastPick) {
+			if (sorter.order === 'asc') {
+				sorter.order = 'desc'
+			} else {
+				sorter.order = 'asc'
 			}
+		}
 
-      if (type !== sorter.lastPick) {
-				setSorter({ order: 'desc', lastPick: type })
-			}
+		if (type !== sorter.lastPick) {
+			sorter.order = 'desc'
+			sorter.lastPick = type
+		}
+    
+		const sort = {
+			time: {
+				asc() {
+					copy.sort((a: Trades, b: Trades) => +a.time - +b.time)
+				},
+				desc() {
+					copy.sort((a: Trades, b: Trades) => +b.time - +a.time)
+				},
+			},
+			price: {
+				asc() {
+					copy.sort((a: Trades, b: Trades) => +a.price - +b.price)
+				},
+				desc() {
+					copy.sort((a: Trades, b: Trades) => +b.price - +a.price)
+				},
+			},
+			quantity: {
+				asc() {
+					copy.sort((a: Trades, b: Trades) => +a.qty - +b.qty)
+				},
+				desc() {
+					copy.sort((a: Trades, b: Trades) => +b.qty - +a.qty)
+				},
+			},
+		}
+		sort[sorter.lastPick][sorter.order]()
 
-      const sort = {
-        time:{ 
-          asc(){
-            copy.sort((a: Trades, b: Trades) => +a.time - +b.time)
-          },
-          desc() {
-            copy.sort((a: Trades, b: Trades) => +b.time - +a.time)
-          }
-         },
-        price: {
-          asc() {
-            copy.sort((a: Trades, b: Trades) => +a.price - +b.price)   
-          },
-          desc() {
-            copy.sort((a: Trades, b: Trades) => +b.price - +a.price)   
-          }
-        },
-        quantity: {
-          asc() {
-            copy.sort((a: Trades, b: Trades) => +a.qty - +b.qty)   
-          },
-          desc() {
-            copy.sort((a: Trades, b: Trades) => +b.qty - +a.qty)   
-          }
-
-         }
-      }
-      
-      sort[sorter.lastPick][sorter.order]()
-      
-			return copy
-		})
+		setTrades(copy)
 	}
 
 	return (
