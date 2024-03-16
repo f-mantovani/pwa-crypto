83c514c8ff5dfdbc27ece5d8704e34a3eb6cea0f
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2024-03-16 16:57:19 +0100

    config for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    registerType: 'prompt',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-ico.svg'],
+    manifest: {
+        name: 'Crypto Comparison Tool',
+        short_name: 'CryptoComp',
+        description:
+            'A small exercise about fetching data in react using TS and now converting to a PWA',
+        icons: [
+            {
+                src: 'manifest-icon-192.maskable.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'any',
+            },
+            {
+                src: 'manifest-icon-192.maskable.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'maskable',
+            },
+            {
+                src: 'manifest-icon-512.maskable.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any',
+            },
+            {
+                src: 'manifest-icon-512.maskable.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable',
+            },
+        ],
+        theme_color: '#e9dfdf',
+        background_color: '#111111',
+        display: 'fullscreen',
+        start_url: '/',
+        orientation: 'portrait',
+    },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-})
+    plugins: [react(), VitePWA(manifestForPlugin)],
+});
